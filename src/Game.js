7e68397a1c328889af4137c93266315fc6f06b27
mluzarow <mluzarow@users.noreply.github.com>
2018-08-15 21:39:19 -0500

    Added game init.

---
+++ src/Game.js
@@ -0,0 +1,29 @@
+/**
+ * Game initializer.
+ */
+class Game {
+	/**
+	 * Game boot entry point. 
+	 */
+	initialize () {
+		/**
+		 * @type {Element} canvas element
+		 */
+		this.$canvas = document.getElementById ("game");
+		
+		// Probably start the a seperate renderer class which contains the render
+		// loop frame locked to whatever. Split logic and render loops? Does that
+		// make any sense?
+	}
+	
+	/**
+	 * Game loop.
+	 */
+	gameLoop () {
+		// get input
+		// process stuff
+		// render stuff
+		
+		this.gameLoop ();
+	}
+}
