c7936982af99dbbce8a4b45d35939360c2df84b8
Nat34 <nathan.a.thomas@gmail.com>
2018-08-19 15:58:49 -0500

    initial commit and Render class outline

---
+++ src/Render.js
@@ -0,0 +1,18 @@
+/**
+ * Render class.
+ */
+class Render {
+    /**
+     * Update the adventure.
+     */
+	function update () {}
+    /**
+     * Draw the current adventure.
+     */
+    function draw () {}
+    /**
+     * Control the adventure.
+     */
+    function loop () {}
+    
+}
