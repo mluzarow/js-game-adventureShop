bc1c46a5104e1c4048b0bbe9c6eef1ff96bf0bc9
mluzarow <mluzarow@users.noreply.github.com>
2018-09-01 15:04:30 -0500

    Updated spaces -> tabs.

--- src/Core/Render.js
+++ src/Core/Render.js
@@ -2,17 +2,16 @@
 * Render class.
 */
 class Renderer {
-    /**
-    * Update the adventure.
-    */
-    function update () {}
-    /**
-    * Draw the current adventure.
-    */
-    function draw () {}
-    /**
-    * Control the adventure.
-    */
-    function loop () {}
-
+	/**
+	* Update the adventure.
+	*/
+	function update () {}
+	/**
+	* Draw the current adventure.
+	*/
+	function draw () {}
+	/**
+	* Control the adventure.
+	*/
+	function loop () {}
 }
