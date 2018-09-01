d7aa04113ee2994e13f0bd2ca9a1443f9f20a0db
mluzarow <mluzarow@users.noreply.github.com>
2018-09-01 17:19:31 -0500

    Added context prop.

--- src/Core/Renderer.js
+++ src/Core/Renderer.js
@@ -1,7 +1,39 @@
 /**
-* Render class.
+* Renderer controlling processing and output of canvas display.
 */
 class Renderer {
+	/**
+	 * Constructor for renderer.
+	 * 
+	 * @param {Element} $canvas canvas element
+	 */
+	function constructor ($canvas) {
+		/**
+		 * @type {Object} canvas context
+		 */
+		this.context;
+		
+		this.setContext ($canvas);
+	}
+	
+	/**
+	 * Gets the canvas context.
+	 * 
+	 * @type {Object} canvas context
+	 */
+	getContext () {
+		return this.context;
+	}
+	
+	/**
+	 * Sets the canvas context using the element reference.
+	 * 
+	 * @param {Element} $canvas canvas element
+	 */
+	setContext ($canvas) {
+		this.context = $canvas.getContext ("2d");
+	}
+	
 	/**
 	* Update the adventure.
 	*/
